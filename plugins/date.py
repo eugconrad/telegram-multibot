d57360b1f6313d9683269bd6aac01588cfadebf6
conradk10 <orion25rus@gmail.com>
2021-01-02 15:23:07 +0200

    Обновлен метод отображения даты

--- plugins/date.py
+++ plugins/date.py
@@ -7,17 +7,10 @@ import datetime
 @Client.on_message(Filters.regex("^[Tt]oday$")& Filters.me)
 def today(app : Client , msg : Message):
 
-    now = datetime.datetime.today()
-    NY = datetime.datetime(2020,1,1)
-    d = NY-now
-    mm, ss = divmod(d.seconds, 60)
-    hh, mm = divmod(mm, 60)
-
     text = f"""
-\ud83d\udd70 Сейчас на календаре `{datetime.datetime.today().strftime("%d.%m %Y")}`
+\ud83d\udd70 Сейчас на календаре `{datetime.datetime.today().strftime("%d.%m.%Y")}`
+Идёт `{int(datetime.datetime.today().strftime("%j"))}` день `{datetime.datetime.today().strftime("%Y")}` года
 Текущее время `{datetime.datetime.today().strftime("%H часов %M минут")}`
-Идёт `{-d.days}` день `{datetime.datetime.today().strftime("%Y")}` года
-До нового года осталось `{d.days+366}` дней `{hh}` часов и `{mm}` минут! \u2603\ufe0f
 
 **[Multibot by \ud83c\udf52]**
     """
